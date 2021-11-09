2c407d87a6824a5d6078ee9df3e3af60cad96e41
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-09 19:58:59 +0200

    localstorage fixed

--- src/context/theme.js
+++ src/context/theme.js
@@ -4,16 +4,17 @@ import { themeHandler } from "../services";
 
 export const ThemeContext = createContext(null);
 
+const storageName = '"theme-personal-portfolio"';
+
 const ThemeProvider = (props) => {
-  const defaultTheme =
-    localStorage.getItem("theme-personal-portfolio") || "dark";
+  const defaultTheme = localStorage.getItem(storageName) || "dark";
   const [theme, setTheme] = useState(defaultTheme);
   const currentTheme = themeHandler(theme);
 
   const setThemeToggle = () => {
     setTheme((prev) => {
       const _theme = prev === "dark" ? "light" : "dark";
-      localStorage.setItem("theme", _theme);
+      localStorage.setItem(storageName, _theme);
       return _theme;
     });
   };
